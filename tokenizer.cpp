50c6e6667e88f3b5da6c008bf150d01c119c0b98
Ben Lipson <bjlipson@spscommerce.com>
2018-05-17 15:43:29 -0500

    Regex!!

--- tokenizer.cpp
+++ tokenizer.cpp
@@ -1,53 +1,35 @@
+#include <algorithm>
 #include <string>
 #include <vector>
 #include <sstream>
 #include <iostream>
+#include <regex>
 
 int main()
 {
-    std::string src = "func bool isEven(int inputInt) (if (inputInt % 2 = 0) (return True)else (return False))";
+    std::string src = "func bool isEven((int inputInt) (if (inputInt% 2=0 ) (return True)else (return False))";
     std::vector<std::string> tokens;
     std::stringstream spaceCheck(src);
     std::string buffer;
     while (std::getline(spaceCheck, buffer, ' '))
     {
-        unsigned long openParenPos = buffer.find('(');
-        if (openParenPos != std::string::npos)
+        std::smatch matcher;
+        std::regex specialParseChars(R"(\(|\)|\+|-|\*|/|%|\^|>|<|=|&|\||!|:)");
+        while (std::regex_search(buffer, matcher, specialParseChars))
         {
-            if (openParenPos != 0)
+            for (const auto match : matcher)
             {
-                tokens.push_back(buffer.substr(0, openParenPos));
-                tokens.push_back(buffer.substr(openParenPos, 1));
-                tokens.push_back(buffer.substr(openParenPos + 1, (buffer.size() - (openParenPos + 1))));
+                if (matcher.prefix() != "")
+                {
+                    tokens.push_back(matcher.prefix());
+                }
+                tokens.push_back(match);
             }
-            else
-            {
-                tokens.emplace_back("(");
-                tokens.push_back(buffer.substr(1, buffer.size() - 1));
-            }
+            buffer = matcher.suffix().str();
         }
-
-        unsigned long closeParenPos = buffer.find(')');
-        if (closeParenPos != std::string::npos)
+        if (buffer != "" && buffer != " " && buffer != "\n" && buffer != "\t")
         {
-            if (closeParenPos != 0)
-            {
-                tokens.push_back(buffer.substr(0, closeParenPos));
-                tokens.push_back(buffer.substr(closeParenPos, 1));
-            }
-            else
-            {
-                tokens.emplace_back(")");
-                tokens.push_back(buffer.substr(1, buffer.size() - 1));
-            }
-        }
-
-        if (buffer != " " && buffer != "\n" && buffer != "\t")
-        {
-            if (openParenPos == std::string::npos && closeParenPos == std::string::npos)
-            {
-                tokens.push_back(buffer);
-            }
+            tokens.push_back(buffer);
         }
     }
     for (const auto &token : tokens)
